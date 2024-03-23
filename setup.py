ec3df467eacae3bf990ccd45a2c6b9fd4dc16bd0
Theodor Nowicki <theodornowicki@gmail.com>
2024-03-23 12:09:23 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+import sys
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {
+    "include_files" : ["error_sound.ogg"]}
+
+
+setup(
+    name="Stroop Test Version 2 Cue zuerst",
+    version="0.1",
+    description="Stroop test Cue zuerst",
+    options={"build_exe": build_exe_options},
+    executables=[Executable("stroop_program.py",base="Win32GUI" )],
+)
+
+# put in console: python setup.py bdist_msi
